2da5492f16833b9ab79cf55dc2364ee694992e8d
natnaelawel <natnael.awel@gmail.com>
2020-08-09 11:46:32 +0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <v-app>
+       <v-content>
+           <v-container>
+               Hello World
+               <v-btn depressed small color="primary">Primary</v-btn>
+           </v-container>
+       </v-content>
+   </v-app>
+</div>
+@endsection
